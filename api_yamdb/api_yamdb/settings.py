b3194744ce7b783b3294bffaa04581eade0e639e
Александр Морозов <alex.docs1988@gmail.com>
2023-05-25 15:19:27 +0300

    SECRET_KEY

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,9 +8,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
-
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 if SECRET_KEY is None:
     sys.exit('ERROR: SECRET_KEY не найден в среде окружения.')
